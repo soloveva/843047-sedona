10ecaa2ff370679d65fce1591a7923df5f4701c4
soloveva <ale-na4ka@yandex.ru>
2018-09-09 00:20:16 +0300

    Корректировочки.

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,11 @@ var child = popup.querySelector("[name=amount-adult-child]");
 var storage1 = localStorage.getItem("adult");
 var storage2 = localStorage.getItem("child");
 
-try {
-  storage = localStorage.getItem("login");
-} catch (err) {
-  isStorageSupport = false;
-}
-
 search.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-open");
+  popup.classList.toggle("modal-open");
+  popup.classList.toggle("modal-close");
   datestart.focus();
   if (storage1) {
     adult.value = storage1;
@@ -24,12 +20,6 @@ search.addEventListener("click", function (evt) {
   if (storage2) {
     child.value = storage2;
   }
-});
-
-search.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("modal-open");
-  popup.classList.toggle("modal-close");
 });
 
 popup.addEventListener("submit", function (evt) {
